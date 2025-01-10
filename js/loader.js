ecf39b848f00bcd0ba59ba679f8a9a17cff1c06a
adask.business@gmail.com <adask.business@gmail.com>
2025-01-10 21:45:05 +0100

    Update loader and main_page

--- js/loader.js
+++ js/loader.js
@@ -1,13 +1,13 @@
 window.addEventListener('load', () => {
-    setTimeout(() => {
-      fetch('main_page.html')
-        .then(response => response.text())
-        .then(fullHTML => {
-          document.open();
-          document.write(fullHTML);
-          document.close();
-        })
-        .catch(err => console.error('Fehler beim Laden der Hauptseite:', err));
-    }, 0);
-  });
-  
+  // Warte 0ms (praktisch direkt), bevor die main_page.html geladen und geschrieben wird.
+  setTimeout(() => {
+    fetch('main_page.html')
+      .then(response => response.text())
+      .then(fullHTML => {
+        document.open();
+        document.write(fullHTML);
+        document.close();
+      })
+      .catch(err => console.error('Fehler beim Laden der Hauptseite:', err));
+  }, 0);
+});
