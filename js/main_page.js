9fe5edd7bb61cc0caacea1c1529f9f790c66b3e8
adask.business@gmail.com <adask.business@gmail.com>
2025-01-12 09:36:05 +0100

    Update Test

--- js/main_page.js
+++ js/main_page.js
@@ -193,10 +193,10 @@
     }
   });
 
-  /* flip*/
+  /* flip
   
   document.querySelectorAll('.flip-card').forEach((card) => {
     card.addEventListener('click', () => {
       card.querySelector('.flip-card-inner').classList.toggle('is-flipped');
     });
-  });
+  });*/
